a25e4d903652768882f964a85c6a3b2f19f6ffea
Michael A <mandre@jrn.com>
2015-02-11 16:04:58 -0600

    design

--- static/questions.js
+++ static/questions.js
@@ -3,90 +3,15 @@ var temp1 = {
   number : 1,
   question: "Oshkosh Corp. earnings fell ___ on decline in military vehicle sales",
   options: {
-    a: "10%",
-    b: "20%",
-    c: "30%",
-    d: "40%"
+    0: "10%",
+    1: "20%",
+    2: "30%",
+    3: "40%"
   },
-  answer: "d",
+  answer: 3,
   answerDetails: "With a 44% drop in military vehicle revenue, Oshkosh Corp. on Tuesday reported a sharp decline in earnings for its fiscal first quarter but exceeded analysts' expectations. The company posted net income of $34.6 million, or 43 cents a share, down 40% from $54.7 million, or 63 cents, a year earlier.",
   imgsrc: "http://media.jrn.com/images/660*475/b99433721z.1_20150127210916_000_gse9n1jq.1-0.jpg",
   articleTitle: "Oshkosh Corp. earnings fall",
   articleUrl: "http://www.jsonline.com/business/oshkosh-corp-earnings-fall-40-on-decline-in-military-vehicle-sales-b99433721z1-289905881.html"
 };
-var temp2 = {
-  number: 2,
-  question: "Walker proposes investment of ____ million for Bucks arena",
-  options: {
-    a: "$120",
-    b: "$220",
-    c: "$320",
-    d: "$420"
-  },
-  answer: "b",
-  answerDetails: "This could be a long winded explanation. ",
-  imgsrc: "",
-  articleTitle: "Read more...",
-  articleUrl: "http://www.jsonline.com/news/statepolitics/walker-jock-tax-will-cover-220-million-for-new-bucks-arena-b99433734z1-289935421.html"
-};
-var temp3 = {
-  number : 3,
-  question: "Oshkosh Corp. earnings fell ___ on decline in military vehicle sales",
-  options: {
-    a: "10%",
-    b: "20%",
-    c: "30%",
-    d: "40%"
-  },
-  answer: "d",
-  answerDetails: "With a 44% drop in military vehicle revenue, Oshkosh Corp. on Tuesday reported a sharp decline in earnings for its fiscal first quarter but exceeded analysts' expectations. The company posted net income of $34.6 million, or 43 cents a share, down 40% from $54.7 million, or 63 cents, a year earlier.",
-  imgsrc: "http://media.jrn.com/images/660*475/b99433721z.1_20150127210916_000_gse9n1jq.1-0.jpg",
-  articleTitle: "Oshkosh Corp. earnings fall",
-  articleUrl: "http://www.jsonline.com/business/oshkosh-corp-earnings-fall-40-on-decline-in-military-vehicle-sales-b99433721z1-289905881.html"
-};
-var temp4 = {
-  number: 4,
-  question: "Walker proposes investment of ____ million for Bucks arena",
-  options: {
-    a: "$120",
-    b: "$220",
-    c: "$320",
-    d: "$420"
-  },
-  answer: "b",
-  answerDetails: "This could be a long winded explanation. ",
-  imgsrc: "",
-  articleTitle: "Read more...",
-  articleUrl: "http://www.jsonline.com/news/statepolitics/walker-jock-tax-will-cover-220-million-for-new-bucks-arena-b99433734z1-289935421.html"
-};
-var temp5 = {
-  number : 5,
-  question: "Oshkosh Corp. earnings fell ___ on decline in military vehicle sales",
-  options: {
-    a: "10%",
-    b: "20%",
-    c: "30%",
-    d: "40%"
-  },
-  answer: "d",
-  answerDetails: "With a 44% drop in military vehicle revenue, Oshkosh Corp. on Tuesday reported a sharp decline in earnings for its fiscal first quarter but exceeded analysts' expectations. The company posted net income of $34.6 million, or 43 cents a share, down 40% from $54.7 million, or 63 cents, a year earlier.",
-  imgsrc: "http://media.jrn.com/images/660*475/b99433721z.1_20150127210916_000_gse9n1jq.1-0.jpg",
-  articleTitle: "Oshkosh Corp. earnings fall",
-  articleUrl: "http://www.jsonline.com/business/oshkosh-corp-earnings-fall-40-on-decline-in-military-vehicle-sales-b99433721z1-289905881.html"
-};
-var temp6 = {
-  number: 6,
-  question: "Walker proposes investment of ____ million for Bucks arena",
-  options: {
-    a: "$120",
-    b: "$220",
-    c: "$320",
-    d: "$420"
-  },
-  answer: "b",
-  answerDetails: "This could be a long winded explanation. ",
-  imgsrc: "",
-  articleTitle: "Read more...",
-  articleUrl: "http://www.jsonline.com/news/statepolitics/walker-jock-tax-will-cover-220-million-for-new-bucks-arena-b99433734z1-289935421.html"
-};
-questionsArray.push(temp1,temp2,temp3,temp4,temp5,temp6);
+questionsArray.push(temp1);
