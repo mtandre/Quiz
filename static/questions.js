a4105bc1804af357d4c39eedc9e19b1d7c98eb34
Michael A <mandre@jrn.com>
2015-02-12 09:05:53 -0600

    styled up

--- static/questions.js
+++ static/questions.js
@@ -1,6 +1,19 @@
 var questionsArray = [];
 var temp1 = {
-  number : 1,
+  question: "Which Milwaukee Brewer injured his hamstring this week?",
+  options: {
+    0: "Ryan Braun",
+    1: "Jonathan Lucroy",
+    2: "Rob Deer",
+    3: "Hank the dog"
+  },
+  answer: 1,
+  cid: "291522811",
+  answerDetails: "The Milwaukee Brewers didn't make it to spring training before suffering their first injury of 2015. The team announced Wednesday morning that catcher Jonathan Lucroy, one of the key players on the club, is expected to be sidelined four to six...",
+  imgsrc: "http://media.jsonline.com/images/38106706-mjs_477588083hh00007_milwaukee_.jpg",
+  articleUrl: "http://www.jsonline.com/blogs/sports/291522811.html"
+};
+var temp2 = {
   question: "Oshkosh Corp. earnings fell ___ on decline in military vehicle sales",
   options: {
     0: "10%",
@@ -9,9 +22,79 @@ var temp1 = {
     3: "40%"
   },
   answer: 3,
+  cid: "",
   answerDetails: "With a 44% drop in military vehicle revenue, Oshkosh Corp. on Tuesday reported a sharp decline in earnings for its fiscal first quarter but exceeded analysts' expectations. The company posted net income of $34.6 million, or 43 cents a share, down 40% from $54.7 million, or 63 cents, a year earlier.",
   imgsrc: "http://media.jrn.com/images/660*475/b99433721z.1_20150127210916_000_gse9n1jq.1-0.jpg",
-  articleTitle: "Oshkosh Corp. earnings fall",
+  articleUrl: "http://www.jsonline.com/business/oshkosh-corp-earnings-fall-40-on-decline-in-military-vehicle-sales-b99433721z1-289905881.html"
+};
+var temp3 = {
+  question: "Oshkosh Corp. earnings fell ___ on decline in military vehicle sales",
+  options: {
+    0: "10%",
+    1: "20%",
+    2: "30%",
+    3: "40%"
+  },
+  answer: 3,
+  cid: "",
+  answerDetails: "With a 44% drop in military vehicle revenue, Oshkosh Corp. on Tuesday reported a sharp decline in earnings for its fiscal first quarter but exceeded analysts' expectations. The company posted net income of $34.6 million, or 43 cents a share, down 40% from $54.7 million, or 63 cents, a year earlier.",
+  imgsrc: "http://media.jrn.com/images/660*475/b99433721z.1_20150127210916_000_gse9n1jq.1-0.jpg",
+  articleUrl: "http://www.jsonline.com/business/oshkosh-corp-earnings-fall-40-on-decline-in-military-vehicle-sales-b99433721z1-289905881.html"
+};
+var temp4 = {
+  question: "Oshkosh Corp. earnings fell ___ on decline in military vehicle sales",
+  options: {
+    0: "10%",
+    1: "20%",
+    2: "30%",
+    3: "40%"
+  },
+  answer: 3,
+  cid: "",
+  answerDetails: "With a 44% drop in military vehicle revenue, Oshkosh Corp. on Tuesday reported a sharp decline in earnings for its fiscal first quarter but exceeded analysts' expectations. The company posted net income of $34.6 million, or 43 cents a share, down 40% from $54.7 million, or 63 cents, a year earlier.",
+  imgsrc: "http://media.jrn.com/images/660*475/b99433721z.1_20150127210916_000_gse9n1jq.1-0.jpg",
+  articleUrl: "http://www.jsonline.com/business/oshkosh-corp-earnings-fall-40-on-decline-in-military-vehicle-sales-b99433721z1-289905881.html"
+};
+var temp5 = {
+  question: "Oshkosh Corp. earnings fell ___ on decline in military vehicle sales",
+  options: {
+    0: "10%",
+    1: "20%",
+    2: "30%",
+    3: "40%"
+  },
+  answer: 3,
+  cid: "",
+  answerDetails: "With a 44% drop in military vehicle revenue, Oshkosh Corp. on Tuesday reported a sharp decline in earnings for its fiscal first quarter but exceeded analysts' expectations. The company posted net income of $34.6 million, or 43 cents a share, down 40% from $54.7 million, or 63 cents, a year earlier.",
+  imgsrc: "http://media.jrn.com/images/660*475/b99433721z.1_20150127210916_000_gse9n1jq.1-0.jpg",
+  articleUrl: "http://www.jsonline.com/business/oshkosh-corp-earnings-fall-40-on-decline-in-military-vehicle-sales-b99433721z1-289905881.html"
+};
+var temp6 = {
+  question: "Oshkosh Corp. earnings fell ___ on decline in military vehicle sales",
+  options: {
+    0: "10%",
+    1: "20%",
+    2: "30%",
+    3: "40%"
+  },
+  answer: 3,
+  cid: "",
+  answerDetails: "With a 44% drop in military vehicle revenue, Oshkosh Corp. on Tuesday reported a sharp decline in earnings for its fiscal first quarter but exceeded analysts' expectations. The company posted net income of $34.6 million, or 43 cents a share, down 40% from $54.7 million, or 63 cents, a year earlier.",
+  imgsrc: "http://media.jrn.com/images/660*475/b99433721z.1_20150127210916_000_gse9n1jq.1-0.jpg",
+  articleUrl: "http://www.jsonline.com/business/oshkosh-corp-earnings-fall-40-on-decline-in-military-vehicle-sales-b99433721z1-289905881.html"
+};
+var temp7 = {
+  question: "Oshkosh Corp. earnings fell ___ on decline in military vehicle sales",
+  options: {
+    0: "10%",
+    1: "20%",
+    2: "30%",
+    3: "40%"
+  },
+  answer: 3,
+  cid: "",
+  answerDetails: "With a 44% drop in military vehicle revenue, Oshkosh Corp. on Tuesday reported a sharp decline in earnings for its fiscal first quarter but exceeded analysts' expectations. The company posted net income of $34.6 million, or 43 cents a share, down 40% from $54.7 million, or 63 cents, a year earlier.",
+  imgsrc: "http://media.jrn.com/images/660*475/b99433721z.1_20150127210916_000_gse9n1jq.1-0.jpg",
   articleUrl: "http://www.jsonline.com/business/oshkosh-corp-earnings-fall-40-on-decline-in-military-vehicle-sales-b99433721z1-289905881.html"
 };
 questionsArray.push(temp1);
