8d01c3a85e792e786bce6eea6c760b874d51aa4c
Michael A <mandre@jrn.com>
2015-02-27 09:01:14 -0600

    bugs: read more tracking & review change answers

--- static/questions-template.tpl
+++ static/questions-template.tpl
@@ -1,24 +1,24 @@
 <div class="q-questions">
   <div class="q-main">
-    <h5 class="q-question"><%= question %></h5>
+    <h5 class="q-question"><%= question.question %></h5>
     <ul class="q-options">
-      <% _.each(options, function(value, key){ %>
-        <li class="q-option<% if(key == answer) print(' q-correct') %>">
+      <% _.each(question.options, function(value, key){ %>
+        <li class="q-option<% if(key == question.answer) print(' q-correct') %>">
           <span class="q-value" data-key="<%= key %>"><%= value %></span>
         </li>
       <% }) %>
     </ul>
   </div>
-  <% if(imgsrc !== '' || answerDetails !== '') { %>
+  <% if(question.imgsrc !== '' || question.answerDetails !== '') { %>
     <div class="q-meta">
-      <% if(imgsrc !== '') { %>
-        <img src="<%= imgsrc %>" <%= hidePhoto ? 'style="opacity:0;"' : ''%>>
+      <% if(question.imgsrc !== '') { %>
+        <img src="<%= question.imgsrc %>" <%= question.hidePhoto ? 'style="opacity:0;"' : ''%>>
       <% } %>
-      <% if(articleUrl !== '' || answerDetails !== '') { %>
+      <% if(question.articleUrl !== '' || question.answerDetails !== '') { %>
         <div class="q-answer-details">
-          <%= answerDetails %>
-          <% if(articleUrl !== '') { %>
-          <a target="_blank" href="<%= articleUrl %>">Read more</a>
+          <%= question.answerDetails %>
+          <% if(question.articleUrl !== '') { %>
+          <a target="_blank" onclick="if(typeof sendGAevents == 'function') {sendGAevents('JSO Quiz', 'Read More', '<%= (index+1) %>');};" href="<%= question.articleUrl %>">Read more</a>
           <% } %>
         </div>
       <% } %>
--- static/templates.js
+++ static/templates.js
@@ -6,35 +6,37 @@ var __t, __p = '', __e = _.escape, __j = Array.prototype.join;
 function print() { __p += __j.call(arguments, '') }
 with (obj) {
 __p += '<div class="q-questions">\n  <div class="q-main">\n    <h5 class="q-question">' +
-((__t = ( question )) == null ? '' : __t) +
+((__t = ( question.question )) == null ? '' : __t) +
 '</h5>\n    <ul class="q-options">\n      ';
- _.each(options, function(value, key){ ;
+ _.each(question.options, function(value, key){ ;
 __p += '\n        <li class="q-option';
- if(key == answer) print(' q-correct') ;
-__p += '">\n          <span class="value" data-key="' +
+ if(key == question.answer) print(' q-correct') ;
+__p += '">\n          <span class="q-value" data-key="' +
 ((__t = ( key )) == null ? '' : __t) +
 '">' +
 ((__t = ( value )) == null ? '' : __t) +
 '</span>\n        </li>\n      ';
  }) ;
 __p += '\n    </ul>\n  </div>\n  ';
- if(imgsrc !== '' || answerDetails !== '') { ;
+ if(question.imgsrc !== '' || question.answerDetails !== '') { ;
 __p += '\n    <div class="q-meta">\n      ';
- if(imgsrc !== '') { ;
+ if(question.imgsrc !== '') { ;
 __p += '\n        <img src="' +
-((__t = ( imgsrc )) == null ? '' : __t) +
+((__t = ( question.imgsrc )) == null ? '' : __t) +
 '" ' +
-((__t = ( hidePhoto ? 'style="opacity:0;"' : '')) == null ? '' : __t) +
+((__t = ( question.hidePhoto ? 'style="opacity:0;"' : '')) == null ? '' : __t) +
 '>\n      ';
  } ;
 __p += '\n      ';
- if(articleUrl !== '' || answerDetails !== '') { ;
+ if(question.articleUrl !== '' || question.answerDetails !== '') { ;
 __p += '\n        <div class="q-answer-details">\n          ' +
-((__t = ( answerDetails )) == null ? '' : __t) +
+((__t = ( question.answerDetails )) == null ? '' : __t) +
 '\n          ';
- if(articleUrl !== '') { ;
-__p += '\n          <a target="_blank" href="' +
-((__t = ( articleUrl )) == null ? '' : __t) +
+ if(question.articleUrl !== '') { ;
+__p += '\n          <a target="_blank" onclick="if(typeof sendGAevents == \'function\') {sendGAevents(\'JSO Quiz\', \'Read More\', \'' +
+((__t = ( (index+1) )) == null ? '' : __t) +
+'\');};" href="' +
+((__t = ( question.articleUrl )) == null ? '' : __t) +
 '">Read more</a>\n          ';
  } ;
 __p += '\n        </div>\n      ';
